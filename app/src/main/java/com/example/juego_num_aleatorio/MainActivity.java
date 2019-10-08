6259d954aa9007476a37f674a002fb4f11eb13e4
SergioPujol98 <sergiopujol.98@gmail.com>
2019-10-08 15:11:58 +0200

    TercerCommit

--- app/src/main/java/com/example/juego_num_aleatorio/MainActivity.java
+++ app/src/main/java/com/example/juego_num_aleatorio/MainActivity.java
@@ -15,10 +15,11 @@ public class MainActivity extends AppCompatActivity {
 
     protected EditText texto;
     protected Button enviar;
-    protected TextView tv = findViewById(R.id.textView3);
+    protected TextView tv;
     int min = 0;
     int max = 100;
     int n = new Random().nextInt((max - min) + 1) + min;
+    int nm;
 
     int intentos = 0;
     int puntuacion = 10;
@@ -26,45 +27,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        tv = findViewById(R.id.textView3);
         texto = findViewById(R.id.editText3);
-        String num1 = texto.getText().toString();
-        final int nm = Integer.parseInt(num1);
+
         enviar = findViewById(R.id.button);
+
         String adiv = "El numero a adivinar es: " + n;
         tv.setText(adiv);
         enviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                boolean falso = false;
-                while (!falso) {
-
+                nm = Integer.parseInt(texto.getText().toString());
                     if (nm == n) {
                         String s2 = ("Has adivinado el numero, FELICIDADES. \nTu puntuacion es: " + puntuacion);
                         Toast.makeText(getApplicationContext(), s2, Toast.LENGTH_SHORT).show();
-                        tv.setText(puntuacion);
-                        falso = true;
+
                     } else {
                         intentos++;
                         puntuacion--;
                         if (nm < n) {
-                            String s1 = ("Fallos: " + intentos + "\nPuntuacion: " + puntuacion + "\nEl numero es mayor al introducido");
+                            String s1 = ("Fallos: " + intentos + "\nPuntuacion: " + puntuacion + "\nEl numero es mayor al introducido" +n);
                             Toast.makeText(getApplicationContext(), s1, Toast.LENGTH_SHORT).show();
                         } else {
                             String s1 = ("Fallos: " + intentos + "\nPuntuacion: " + puntuacion + "\nEl numero es menor al introducido");
                             Toast.makeText(getApplicationContext(), s1, Toast.LENGTH_SHORT).show();
                         }
-                        String s1 = ("Fallos: " + intentos + "\nPuntuacion: " + puntuacion);
-                        Toast.makeText(getApplicationContext(), s1, Toast.LENGTH_SHORT).show();
+
                     }
                     if (puntuacion == 0) {
                         String s3 = ("Has perdido, el numero era el: " + n);
                         Toast.makeText(getApplicationContext(), s3, Toast.LENGTH_SHORT).show();
-                        break;
+
                     }
                 }
-            }
+
         });
     }
 }
